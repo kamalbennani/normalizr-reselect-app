bd964ae0f9dcab361f60170edfabf74ece986cf4
Kamal Bennani <kamalbouchboy1@hotmail.fr>
2017-09-27 10:22:09 +0200

    make redux available outside of the app (redux-dev-tools)

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-let store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
