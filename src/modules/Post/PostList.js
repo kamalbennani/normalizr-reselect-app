d9f97eec1710b085afc55ff5e4fbe1f7dd306b55
Kamal Bennani <kamalbouchboy1@hotmail.fr>
2017-09-27 15:00:32 +0200

    handle ui

--- src/modules/Post/PostList.js
+++ src/modules/Post/PostList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid, List, Card } from 'semantic-ui-react';
 import PostItem from './PostItem';
 
 class PostList extends Component {
@@ -8,11 +8,9 @@ class PostList extends Component {
     const { postIds } = this.props;
     return (
       <Grid centered padded columns={2}>
-        <Grid.Column>
-          <List selection>
-            {postIds.map(postId => <PostItem key={postId} postId={postId} handleItemClick={() => this.handleItemClick(postId)}/>)}
-          </List>
-        </Grid.Column>
+        <Card.Group>
+          {postIds.map(postId => <PostItem key={postId} postId={postId} handleItemClick={() => this.handleItemClick(postId)}/>)}
+        </Card.Group>
       </Grid>
     )
   }
